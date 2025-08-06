ac349bc28b0da360b4d7f60496f2ecce333759f9
Is an English Major <Najeebihesa@gmail.com>
2025-08-06 16:59:39 +0300

    ..

--- scripts/dom-calc.js
+++ scripts/dom-calc.js
@@ -1,25 +1,84 @@
 
-// lets put the consts here 
-const buttonNumElm = document.getElementById('#button number')
-const buttonOpClkElm = document.getElementById('#button operator')
-const displayElm = document.getElementById('#display')
+//--------------------------------CONSTANTS-----------------------//
+const calculator = document.querySelector('.calculator')
+const buttonNumElm = document.querySelectorAll('.number')
+const operatorClkElm = document.querySelectorAll('.operator')
+const displayElm = document.querySelector('.display')
 console.log()
-
-
+//---------------------------------VARIABLES---------------------//
+let currentInput = ""
+let operator = ""
+let numOne = ""
+let numTwo = ""
+let display = ""
+//-----------------------------------FUNCTIONS-------------------//
 //put your functions here 
-function init(user, numOne,numTwo) {
-if (user === "+"){
-    return(numOne + numTwo)
-} else if (user === "-"){
-    return (numOne - numTwo)
-} else if (user === "*"){
-return (numOne * numTwo)
-} else if (user === '/') {
-    return (numOne / numTwo)
+function init() {
 }
-}
-function buttonNumElm(){
-return display.value += input
+
+function clickHandler(){
 }
 //event listeners
+buttonNumElm.forEach((oneElement)=> {
+    oneElement.addEventListener('click', (event) => {
+    console.log(oneElement.textContent)
+        
+   
+    if(numOne && operator){
+    numTwo = event.target.textContent
+    }
+    else if(numOne && !operator ){
+        numOne+= event.target.textContent
+
+    }
+     else { 
+        numOne = event.target.textContent
+    }
+    // console.log(event.target.innerText)
+    console.log("num1 ", numOne)
+    console.log("operator ", operator)
+    console.log("num2 ", numTwo)
+
+})
+ })
+
+
+
+operatorClkElm.forEach((oneOperation)=>{
+    oneOperation.addEventListener('click',(event)=>{
+    console.log(oneOperation.textContent)
+    
+    
+    if (numOne){
+    operator = event.target.textContent
+
+    }
+    console.log(operator)
+
+     if(event.target.textContent === "="){
+        console.log("=======")
+    }
+       
+    })
+
+
+   
+})
+
+
+
 document.addEventListener('DOMContentLoaded', init)
+
+
+
+
+/* 
+
+1. get the equals element from the dom html
+
+2. attach an onclick event to it that when clicked console log
+
+3. check the operator and based on what it is perform that operation on both numbers and console.log the result
+
+4. display the result in the page
+*/
